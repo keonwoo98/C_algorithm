d81e217dadd3f52e93fd53ce080a6d32259caba2
Keonwoo <keonwoo98@naver.com>
2021-01-25 23:06:45 +0900

    ch03

--- ch01-ch05/ch03.c
+++ ch01-ch05/ch03.c
@@ -11,7 +11,65 @@
 //         if(a[i]==key)
 //             break;
 //     }
-//     return i==key ? -1 : i;
+//     return i==n ? -1 : i;
+// }
+
+// int main(void)
+// {
+//     int n;
+//     int key;
+//     int idx;
+//     printf("배열의 길이: ");
+//     scanf("%d", &n);
+//     int *x = calloc(n+1, sizeof(int));
+
+//     for(int i=0; i<n; i++)
+//     {
+//         printf("x[%d]: ", i);
+//         scanf("%d", &x[i]);
+//     }
+
+//     printf("찾으려는 배열의 요소: ");
+//     scanf("%d", &key);
+//     idx = search(x, n, key);
+//     if(idx == -1)
+//         puts("search fail!");
+//     else
+//         printf("%d는 x[%d]\n", key, idx);
+//     free(x);
+//     return 0;
+// }
+
+// Q2
+// int search(const int a[], int n, int key)
+// {
+// 	int i;
+
+// 	printf("   |");
+// 	for (i = 0; i < n; i++)
+// 		printf("%3d", i);
+// 	printf("\n---+");
+// 	for (i = 0; i < n; i++)
+// 		printf("---");
+// 	printf("-\n");
+
+// 	for (i = 0; i < n; i++) {
+// 		int j;
+// 		printf("   |");
+// 		printf("%*s", 3 * (i - 1) + 5, "");
+// 		printf("*\n");
+// 		printf("%3d|", i);
+// 		for (j = 0; j < n; j++)
+// 			printf("%3d", a[j]);
+// 		putchar('\n');
+
+// 		if (a[i] == key)
+// 			return i; /* 검색 성공 */
+
+// 		printf("   |\n");
+// 	}
+
+// 	return -1; /* 검색 실패 */
 // }
 
 // int main(void)
@@ -30,65 +88,232 @@
 
 //     printf("찾으려는 배열의 요소: ");
 //     scanf("%d", &key);
-//     printf("%d는 x[%d]\n", key, search(x, n, key));
+// 	    int idx = search(x, n, key);	/* 배열 x에서 값이 ky 인 요소를 선형 검색 */
+
+// 	   if (idx == -1)
+// 		    puts("검색에 실패했습니다.");
+// 	   else
+// 		    printf("%d는 x[%d]에 있습니다.\n", key, idx);
 //     free(x);
 //     return 0;
 // }
 
-// Q2
-int search(const int a[], int n, int key)
-{
-	int i;
+// 실습 3-4
+// int bin_search(const int a[], int n, int key)
+// {
+//     int pl = 0;
+//     int pr = n-1;
+//     int pc;
+//     do
+//     {
+//         pc = (pl+pr) / 2;
+//         if(a[pc] == key)
+//             return pc;
+//         else if(a[pc] < key)
+//             pl = pc + 1;
+//         else
+//             pr = pc - 1;
+//     } while (pl <= pr);
+//     return -1;
+    
+// }
 
-	printf("   |");
-	for (i = 0; i < n; i++)
-		printf("%3d", i);
-	printf("\n---+");
-	for (i = 0; i < n; i++)
-		printf("---");
-	printf("-\n");
+// int main(void)
+// {
+//     int i, nx, ky, idx;
+//     int *x;
+//     puts("이진 검색");
+//     printf("요소 개수: ");
+//     scanf("%d", &nx);
+//     x = calloc(nx, sizeof(int));
+//     printf("오름차운으로 입력하세요.\n");
+//     printf("x[0]: ");
+//     scanf("%d", &x[0]);
+//     for(i=1; i<nx; i++)
+//     {
+//         do
+//         {
+//             printf("x[%d]: ", i);
+//             scanf("%d", &x[i]);
+//         } while(x[i] < x[i-1]);
+//     }
+//     printf("검색값: ");
+//     scanf("%d", &ky);
+//     idx = bin_search(x, nx, ky);
+//     if(idx == -1)
+//         puts("검색에 실패했습니다.");
+//     else
+//         printf("%d는(은) x[%d]에 있습니다\n", ky, idx);
+//     free(x);
+//     return 0;
+// }
 
-	for (i = 0; i < n; i++) {
-		int j;
-		printf("   |");
-		printf("%*s", 3 * (i - 1) + 5, "");
-		printf("*\n");
-		printf("%3d|", i);
-		for (j = 0; j < n; j++)
-			printf("%3d", a[j]);
-		putchar('\n');
+// Q3
+// void search_idx(const int a[], int n, int key, int idx[])
+// {
+//     int j = 0;
+//     for(int i=0; i<n; i++)
+//     {
+//         if(a[i] == key)
+//         {
+//             idx[j] = i;
+//             j++;
+//         }
+//     }
+//     if(j == 0)
+//         puts("검색 실패");
+//     else
+//     {
+//         for(int i=0; i<j; i++)
+//             printf("idx[%d]: %d\n", i, idx[i]);
+//         printf("key값 개수: %d", j);
+//     }
+// }
 
-		if (a[i] == key)
-			return i; /* 검색 성공 */
+// int main(void)
+// {
+//     int i, n, key;
+//     int *a;
+//     int *idx;
 
-		printf("   |\n");
-	}
+//     printf("요소 개수: ");
+//     scanf("%d", &n);
+//     a = calloc(n, sizeof(int));
+//     idx = calloc(n, sizeof(int));
+//     for(i=0; i<n; i++)
+//     {
+//         printf("a[%d]: ", i);
+//         scanf("%d", &a[i]);
+//     }
+//     printf("검색값: ");
+//     scanf("%d", &key);
+//     search_idx(a, n, key, idx);
+//     free(a);
+//     free(idx);
+//     return 0;
+// }
 
-	return -1; /* 검색 실패 */
-}
 
-int main(void)
-{
-    int n;
-    int key;
-    printf("배열의 길이: ");
-    scanf("%d", &n);
-    int *x = calloc(n+1, sizeof(int));
+// Q3
+// int search_idx(const int a[], int n, int key, int idx[])
+// {
+// 	int i, count = 0;
 
-    for(int i=0; i<n; i++)
-    {
-        printf("x[%d]: ", i);
-        scanf("%d", &x[i]);
-    }
+// 	for (i = 0; i < n; i++)
+// 		if (a[i] == key)
+// 			idx[count++] = i;
+// 	return count;
+// }
 
-    printf("찾으려는 배열의 요소: ");
-    scanf("%d", &key);
-	int idx = search(x, n, key);	/* 배열 x에서 값이 ky 인 요소를 선형 검색 */
+// int main(void)
+// {
+// 	int i, nx, ky;
+// 	int no;		/* ky과 일치하는 요소의 개수 */
+// 	int *x;		/* 데이터를 저장하는 배열 */
+// 	int *idx;	/* ky와 일치하는 요소의 인덱스를 저장하는 배열 */
 
-	if (idx == -1)
-		puts("검색에 실패했습니다.");
-	else
-		printf("%d는 x[%d]에 있습니다.\n", key, idx);
-    free(x);
-    return 0;
-}
+// 	puts("선형 검색");
+// 	printf("요솟수 : ");
+// 	scanf("%d", &nx);
+// 	x = calloc(nx, sizeof(int));	/* 요솟수 nx인 int 형 배열을 생성 */
+// 	idx = calloc(nx, sizeof(int));	/* 요솟수 nx인 int 형 배열을 생성 */
+
+// 	for (i = 0; i < nx; i++) {
+// 		printf("x[%d] : ", i);
+// 		scanf("%d", &x[i]);
+// 	}
+// 	printf("검색 값 :");
+// 	scanf("%d", &ky);
+
+// 	no = search_idx(x, nx, ky, idx);	/* 배열 x에서 값이 ky 인 요소를 선형 검색 */
+
+// 	if (no == -1)
+// 		puts("일치하는 요소는 존재하지 않습니다.");
+// 	else {
+// 		printf("일치하는 요소는 %d 개입니다.\n", no);
+// 		for (i = 0; i < no; i++)
+// 			printf("x[%d] ", idx[i]);
+// 		putchar('\n');
+// 	}
+
+// 	free(x);	/* 배열을 해제 */
+// 	free(idx);	/* 배열을 해제 */
+
+// 	return 0;
+// }
+
+// Q4
+// int search(const int a[], int n, int key)
+// {
+//     int i;
+//     int pl = 0;
+//     int pr = n - 1;
+//     int pc;
+
+//     printf("   |");
+//     for(i = 0; i < n; i++)
+//         printf("%4d", i);
+//     printf("\n---+");
+//     for(i = 0; i < n; i++)
+//         printf("----");
+//     printf("--\n");
+
+//     do
+//     {
+//         pc = (pl + pr) / 2;
+
+//         printf("   |");
+//         for(i = 0; i < 4 * pl; i++)
+//             printf(" ");
+//         printf(" <-");
+//         for(i = 0; i < 4 * (pc - pl); i++)
+//             printf(" ");
+//         printf("+");
+//         for(i = 0; i < 4 * (pr - pc); i++)
+//             printf(" ");
+//         printf("->\n");
+        
+//         printf("%3d|", pc);
+//         for(i = 0; i < n; i++)
+//             printf("%4d", a[i]);
+//         putchar('\n');
+
+//         if(a[pc] == key)
+//             return pc;
+//         else if(a[pc] < key)
+//             pl = pc + 1;
+//         else
+//             pr = pc - 1;
+//     } while (pl <= pr);
+
+//     return -1;
+// }
+
+// int main(void)
+// {
+//     int n;
+//     int key;
+//     int idx;
+//     printf("배열의 길이: ");
+//     scanf("%d", &n);
+//     int *x = calloc(n, sizeof(int));
+
+//     for(int i = 0; i < n; i++)
+//     {
+//         printf("x[%d]: ", i);
+//         scanf("%d", &x[i]);
+//     }
+//     printf("key: ");
+//     scanf("%d", &key);
+
+//     idx = search(x, n, key);
+//     if(idx == -1)
+//         puts("fail!");
+//     else
+//         printf("%d는 x[%d]에 있습니다\n", key, idx);
+    
+//     free(x);
+//     return 0;
+// }
+
+// Q5
