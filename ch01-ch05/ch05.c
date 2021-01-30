ebd4389839a09272c3d418cab3e792518b911977
Keonwoo <keonwoo98@naver.com>
2021-01-30 20:24:12 +0900

    ch05

--- ch01-ch05/ch05.c
+++ ch01-ch05/ch05.c
@@ -258,59 +258,89 @@
 // }
 
 // Q7
-#include "IntStack.h"
+// #include "IntStack.h"
 
-void move(int no, int x, int y)
+// void move(int no, int x, int y)
+// {
+//     int sw = 0;
+// 	IntStack xstk, ystk, sstk;		/* 스택 */
+
+// 	Initialize(&xstk, 100);
+// 	Initialize(&ystk, 100);
+// 	Initialize(&sstk, 100);
+
+// 	while (1) {
+// 		if (sw == 0 && no > 1) {
+// 			Push(&xstk, x);				/* x 값을 푸시 */
+// 			Push(&ystk, y);				/* y 값을 푸시 */
+// 			Push(&sstk, sw);			/* sw 값을 푸시 */
+// 			no = no - 1;
+// 			y = 6 - x - y;
+// 			continue;
+// 		}
+
+// 		printf("원반[%d]를 %d 기둥에서 %d 기둥으로 이동\n", no, x, y);
+
+// 		if (sw == 1 && no > 1) {
+// 			Push(&xstk, x);				/* x 값을 푸시 */
+// 			Push(&ystk, y);				/* y 값을 푸시 */
+// 			Push(&sstk, sw);			/* sw 값을 푸시 */
+// 			no = no - 1;
+// 			x = 6 - x - y;
+// 			if (++sw == 2) sw = 0;
+// 			continue;
+// 		}
+// 		do {
+// 			if (IsEmpty(&xstk))			/* 스택이 비어있는 상태라면 */
+// 				return;
+// 			Pop(&xstk, &x);				/* x를 팝 */
+// 			Pop(&ystk, &y);				/* y를 팝 */
+// 			Pop(&sstk, &sw);			/* sw를 팝 */
+// 			sw++;
+// 			no++;
+// 		} while (sw == 2);
+// 	}
+
+// 	Terminate(&xstk);
+// 	Terminate(&ystk);
+// 	Terminate(&sstk);
+// }
+
+// int main(void)
+// {
+//     int n;
+//     printf("하노이의 탑\n원반 개수: ");
+//     scanf("%d", &n);
+//     move(n, 1, 3);
+//     return 0;
+// }
+
+// 실습 5-7
+int pos[8];
+
+void print(void)
 {
-    int sw = 0;
-	IntStack xstk, ystk, sstk;		/* 스택 */
+    int i;
+    for(i = 0; i < 8; i++)
+        printf("%2d", pos[i]);
+    putchar('\n');
+}
 
-	Initialize(&xstk, 100);
-	Initialize(&ystk, 100);
-	Initialize(&sstk, 100);
-
-	while (1) {
-		if (sw == 0 && no > 1) {
-			Push(&xstk, x);				/* x 값을 푸시 */
-			Push(&ystk, y);				/* y 값을 푸시 */
-			Push(&sstk, sw);			/* sw 값을 푸시 */
-			no = no - 1;
-			y = 6 - x - y;
-			continue;
-		}
-
-		printf("원반[%d]를 %d 기둥에서 %d 기둥으로 이동\n", no, x, y);
-
-		if (sw == 1 && no > 1) {
-			Push(&xstk, x);				/* x 값을 푸시 */
-			Push(&ystk, y);				/* y 값을 푸시 */
-			Push(&sstk, sw);			/* sw 값을 푸시 */
-			no = no - 1;
-			x = 6 - x - y;
-			if (++sw == 2) sw = 0;
-			continue;
-		}
-		do {
-			if (IsEmpty(&xstk))			/* 스택이 비어있는 상태라면 */
-				return;
-			Pop(&xstk, &x);				/* x를 팝 */
-			Pop(&ystk, &y);				/* y를 팝 */
-			Pop(&sstk, &sw);			/* sw를 팝 */
-			sw++;
-			no++;
-		} while (sw == 2);
-	}
-
-	Terminate(&xstk);
-	Terminate(&ystk);
-	Terminate(&sstk);
+void set(int i)
+{
+    int j;
+    for(j = 0; j < 8; j++)
+    {
+        pos[i] = j;
+        if(i == 7)
+            print();
+        else
+            set(i + 1);
+    }
 }
 
 int main(void)
 {
-    int n;
-    printf("하노이의 탑\n원반 개수: ");
-    scanf("%d", &n);
-    move(n, 1, 3);
+    set(0);
     return 0;
 }
