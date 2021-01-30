d376d72247e7581fe44bc8583ad53fed92f92333
Keonwoo <keonwoo98@naver.com>
2021-01-30 12:41:24 +0900

    ch05

--- ch01-ch05/ch05.c
+++ ch01-ch05/ch05.c
@@ -90,33 +90,123 @@
 // }
 
 // 실습 5-5
-#include "IntStack.h"
+// #include "IntStack.h"
 
-void recur(int n)
+// void recur(int n)
+// {
+//     IntStack stk;
+//     Initialize(&stk, 100);
+// Top :
+//     if(n > 0)
+//     {
+//         Push(&stk, n);
+//         n = n - 1;
+//         goto Top;
+//     }
+//     if(!IsEmpty(&stk))
+//     {
+//         Pop(&stk, &n);
+//         printf("%d\n", n);
+//         n = n - 2;
+//         goto Top;
+//     }
+//     Terminate(&stk);
+// }
+// int main(void)
+// {
+//     int x;
+//     printf("정수를 입력하세요: ");
+//     scanf("%d", &x);
+//     recur(x);
+//     return 0;
+// }
+
+// 실습 5-6
+// void move(int no, int x, int y)
+// {
+//     if(no > 1)
+//         move(no - 1, x, 6 - x- y);
+//     printf("원반[%d]를(을) %d 기둥에서 %d 기둥으로 옮김\n", no, x, y);
+
+//     if(no > 1)
+//         move(no - 1, 6 - x - y, y);
+// }
+// int main(void)
+// {
+//     int n;
+//     printf("하노이의 탑\n원반 개수 : ");
+//     scanf("%d", &n);
+//     move(n, 1, 3);
+//     return 0;
+// }
+
+// recur3
+// void recur3(int n)
+// {
+//     if(n > 0)
+//     {
+//         recur3(n - 1);
+//         recur3(n - 2);
+//         printf("%d\n", n);
+//     }
+// }
+// int main(void)
+// {
+//     int n;
+//     printf("insert n: ");
+//     scanf("%d", &n);
+//     recur3(n);
+//     return 0;
+// }
+
+// Q5
+#include "IntStack.h"
+void recur3(int n)
 {
-    IntStack stk;
-    Initialize(&stk, 100);
-Top :
-    if(n > 0)
-    {
-        Push(&stk, n);
-        n = n - 1;
-        goto Top;
-    }
-    if(!IsEmpty(&stk))
-    {
-        Pop(&stk, &n);
-        printf("%d\n", n);
-        n = n - 2;
-        goto Top;
-    }
-    Terminate(&stk);
+	int sw = 0;
+	IntStack nstk, sstk;			/* 스택 */
+
+	Initialize(&nstk, 100);
+	Initialize(&sstk, 100);
+
+	while (1) {
+		if (n > 0) {
+			Push(&nstk, n);
+			Push(&sstk, sw);
+
+			if (sw == 0)
+				n = n - 1;
+			else if (sw == 1) {
+				n = n - 2;
+				sw = 0;
+			}
+			continue;
+		}
+		do {
+			Pop(&nstk, &n);
+			Pop(&sstk, &sw);
+			sw++;
+
+			if (sw == 2) {
+				printf("%d\n", n);
+				if (IsEmpty(&nstk))
+					return;
+			}
+		} while (sw == 2);
+	}
+
+	Terminate(&nstk);
+	Terminate(&sstk);
 }
+
 int main(void)
 {
-    int x;
-    printf("정수를 입력하세요: ");
-    scanf("%d", &x);
-    recur(x);
-    return 0;
+	int x;
+
+	printf("정수를 입력하세요. : ");
+	scanf("%d", &x);
+
+	recur3(x);
+
+	return 0;
 }
