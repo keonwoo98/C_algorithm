3f8c5f84cce808bb4fd40e02286e57b0080b3708
Keonwoo <keonwoo98@naver.com>
2021-02-01 23:19:34 +0900

    ch05

--- ch01-ch05/ch05.c
+++ ch01-ch05/ch05.c
@@ -346,7 +346,51 @@
 // }
 
 // 실습 5-8
-int flag[8];
+// int flag[8];
+// int pos[8];
+
+// void print(void)
+// {
+//     int i;
+//     for(i = 0; i < 8; i++)
+//         printf("%2d", pos[i]);
+//     putchar('\n');
+// }
+
+// void set(int i)
+// {
+//     int j;
+//     for(j = 0; j < 8; j++)
+//     {
+//         if(!flag[j])
+//         {
+//             pos[i] = j;
+//             if(i == 7)
+//                 print();
+//             else
+//             {
+//                 flag[j] = 1;
+//                 set(i + 1);
+//                 flag[j] = 0;
+//             }
+//         }
+//     }
+// }
+
+// int main(void)
+// {
+//     int i;
+//     for(i = 0; i < 8; i++)
+//         flag[i] = 0;
+//     set(0);
+
+//     return 0;
+// }
+
+// 실습 5-9
+int flag_a[8];
+int flag_b[15];
+int flag_c[15];
 int pos[8];
 
 void print(void)
@@ -362,16 +406,16 @@ void set(int i)
     int j;
     for(j = 0; j < 8; j++)
     {
-        if(!flag[j])
+        if(!flag_a[j] && !flag_b[i + j] && !flag_c[i - j + 7])
         {
             pos[i] = j;
             if(i == 7)
                 print();
             else
             {
-                flag[j] = 1;
+                flag_a[j] = flag_b[i + j] = flag_c[i - j + 7] = 1;
                 set(i + 1);
-                flag[j] = 0;
+                flag_a[j] = flag_b[i + j] = flag_c[i - j + 7] = 0;
             }
         }
     }
@@ -381,7 +425,9 @@ int main(void)
 {
     int i;
     for(i = 0; i < 8; i++)
-        flag[i] = 0;
+        flag_a[i] = 0;
+    for(i = 0; i < 15; i++)
+        flag_b[i] = flag_c[i] = 0;
     set(0);
 
     return 0;
