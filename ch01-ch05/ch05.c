938286cb7fab1decca5bcc0e6fed6c801897cd35
Keonwoo <keonwoo98@naver.com>
2021-02-02 00:56:39 +0900

    ch05

--- ch01-ch05/ch05.c
+++ ch01-ch05/ch05.c
@@ -387,7 +387,59 @@
 //     return 0;
 // }
 
-// 실습 5-9
+// Q8
+// int flag_a[8];
+// int flag_b[15];
+// int flag_c[15];
+// int pos[8];
+
+// void print(void)
+// {
+//     int i, j;
+//     for(i = 0; i < 8; i++)
+//     {
+//         for(j = 0; j < 8; j++)
+//             printf("%s ", j == pos[i] ? "■" : "□");
+//         putchar('\n');
+//     }
+//     putchar('\n');
+// }
+
+// void set(int i)
+// {
+//     int j;
+//     for(j = 0; j < 8; j++)
+//     {
+//         if(!flag_a[j] && !flag_b[i + j] && !flag_c[i - j + 7])
+//         {
+//             pos[i] = j;
+//             if(i == 7)
+//                 print();
+//             else
+//             {
+//                 flag_a[j] = flag_b[i + j] = flag_c[i - j + 7] = 1;
+//                 set(i + 1);
+//                 flag_a[j] = flag_b[i + j] = flag_c[i - j + 7] = 0;
+//             }
+//         }
+//     }
+// }
+
+// int main(void)
+// {
+//     int i;
+//     for(i = 0; i < 8; i++)
+//         flag_a[i] = 0;
+//     for(i = 0; i < 15; i++)
+//         flag_b[i] = flag_c[i] = 0;
+//     set(0);
+
+//     return 0;
+// }
+
+// Q9
+#include "IntStack.h"
+
 int flag_a[8];
 int flag_b[15];
 int flag_c[15];
@@ -395,39 +447,63 @@ int pos[8];
 
 void print(void)
 {
-    int i;
+    int i, j;
     for(i = 0; i < 8; i++)
-        printf("%2d", pos[i]);
+    {
+        for(j = 0; j < 8; j++)
+            printf("%s ", j == pos[i] ? "■" : "□");
+        putchar('\n');
+    }
     putchar('\n');
 }
 
 void set(int i)
 {
     int j;
-    for(j = 0; j < 8; j++)
+    IntStack jstk;
+    Initialize(&jstk, 8);
+
+Start:
+    while (1)
     {
-        if(!flag_a[j] && !flag_b[i + j] && !flag_c[i - j + 7])
+        j = 0;
+        while (1)
         {
-            pos[i] = j;
-            if(i == 7)
-                print();
-            else
+            while (j < 8)
             {
-                flag_a[j] = flag_b[i + j] = flag_c[i - j + 7] = 1;
-                set(i + 1);
-                flag_a[j] = flag_b[i + j] = flag_c[i - j + 7] = 0;
+                if(!flag_a[j] && !flag_b[i + j] && !flag_c[i - j + 7])
+                {
+                    pos[i] = j;
+                    if(i == 7)
+                        print();
+                    else
+                    {
+                        flag_a[j] = flag_b[i + j] = flag_c[i - j + 7] = 1;
+                        i++;
+                        Push(&jstk, j);
+                        goto Start;
+                    }
+                }
+                j++;
             }
+            if(--i == -1) return;
+            Pop(&jstk, &j);
+            flag_a[j] = flag_b[i + j] = flag_c[i - j + 7] = 0;
+            j++;
         }
     }
+    Terminate(&jstk);
 }
 
 int main(void)
 {
     int i;
+
     for(i = 0; i < 8; i++)
         flag_a[i] = 0;
     for(i = 0; i < 15; i++)
         flag_b[i] = flag_c[i] = 0;
+    
     set(0);
 
     return 0;
