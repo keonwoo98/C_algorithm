f0a05e073cfa12f5f239f49ec7d42d3aa446f280
Keonwoo <keonwoo98@naver.com>
2021-02-06 00:34:25 +0900

    ch06

--- ch01-ch06/ch06.c
+++ ch01-ch06/ch06.c
@@ -639,54 +639,125 @@
 // }
 
 // Q11
-void bin_insertion(int a[], int n)
+// void bin_insertion(int a[], int n)
+// {
+// 	int i;
+// 	for (i = 1; i < n; i++) {
+// 		int key = a[i];
+// 		int pl = 0;		
+// 		int pr = i - 1;	
+// 		int pc;			
+// 		int pd;			
+
+// 		do {
+// 			pc = (pl + pr) / 2;
+// 			if (a[pc] == key)		
+// 				break;
+// 			else if (a[pc] < key)
+// 				pl = pc + 1;
+// 			else
+// 				pr = pc - 1;
+// 		} while (pl <= pr);
+// 		pd = (pl <= pr) ? pc + 1 : pr + 1;
+
+// 		memmove(&a[pd + 1], &a[pd], (i - pd) * sizeof(int));
+// 		a[pd] = key;
+// 	}
+// }
+
+// int main(void)
+// {
+// 	int i, nx;
+// 	int *x;		
+
+// 	puts("이진 삽입 정렬");
+// 	printf("요솟수 : ");
+// 	scanf("%d", &nx);
+// 	x = calloc(nx, sizeof(int));
+
+// 	for (i = 0; i < nx; i++) {
+// 		printf("x[%d] : ", i);
+// 		scanf("%d", &x[i]);
+// 	}
+
+// 	bin_insertion(x, nx);		
+
+// 	puts("오름차순으로 정렬했습니다.");
+// 	for (i = 0; i < nx; i++)
+// 		printf("x[%d] = %d\n", i, x[i]);
+
+// 	free(x);	
+
+// 	return 0;
+// }
+
+// 실습 6-6
+// void shell(int a[], int n)
+// {
+//     int i, j, h;
+//     for(h = n / 2; h > 0; h /= 2)
+//         for(i = h; i < n; i++)
+//         {
+//             int tmp = a[i];
+//             for(j = i - h; j >= 0 && a[j] > tmp; j -= h)
+//                 a[j + h] = a[j];
+//             a[j + h] = tmp;
+//         }
+// }
+
+// int main(void)
+// {
+//     int i, nx;
+//     int *x;
+//     puts("셀 정렬");
+//     printf("요소 개수 : ");
+//     scanf("%d", &nx);
+//     x = calloc(nx, sizeof(int));
+//     for(i = 0; i < nx; i++)
+//     {
+//         printf("x[%d] : ", i);
+//         scanf("%d", &x[i]);
+//     }
+//     shell(x, nx);
+//     puts("오름차순으로 정렬했습니다.");
+//     for(i = 0; i < nx; i++)
+//         printf("x[%d] = %d\n", i, x[i]);
+//     free(x);
+//     return 0;
+// }
+
+// 실습 6-7
+void shell(int a[], int n)
 {
-	int i;
-	for (i = 1; i < n; i++) {
-		int key = a[i];
-		int pl = 0;		
-		int pr = i - 1;	
-		int pc;			
-		int pd;			
-
-		do {
-			pc = (pl + pr) / 2;
-			if (a[pc] == key)		
-				break;
-			else if (a[pc] < key)
-				pl = pc + 1;
-			else
-				pr = pc - 1;
-		} while (pl <= pr);
-		pd = (pl <= pr) ? pc + 1 : pr + 1;
-
-		memmove(&a[pd + 1], &a[pd], (i - pd) * sizeof(int));
-		a[pd] = key;
-	}
+    int i, j, h;
+    for(h = 1; h < n / 9; h = h * 3 + 1);
+    for(; h > 0; h /= 3)
+        for(i = h; i < n; i++)
+        {
+            int tmp = a[i];
+            for(j = i - h; j >= 0 && a[j] > tmp; j -= h)
+                a[j + h] = a[j];
+            a[j + h] = tmp;
+        }
 }
 
 int main(void)
 {
-	int i, nx;
-	int *x;		
-
-	puts("이진 삽입 정렬");
-	printf("요솟수 : ");
-	scanf("%d", &nx);
-	x = calloc(nx, sizeof(int));
-
-	for (i = 0; i < nx; i++) {
-		printf("x[%d] : ", i);
-		scanf("%d", &x[i]);
-	}
-
-	bin_insertion(x, nx);		
-
-	puts("오름차순으로 정렬했습니다.");
-	for (i = 0; i < nx; i++)
-		printf("x[%d] = %d\n", i, x[i]);
-
-	free(x);	
-
-	return 0;
+    int i, nx;
+    int *x;
+    puts("셀 정렬");
+    printf("요소 개수 : ");
+    scanf("%d", &nx);
+    x = calloc(nx, sizeof(int));
+    for(i = 0; i < nx; i++)
+    {
+        printf("x[%d] : ", i);
+        scanf("%d", &x[i]);
+    }
+    shell(x, nx);
+    puts("오름차순으로 정렬했습니다.");
+    for(i = 0; i < nx; i++)
+        printf("x[%d] = %d\n", i, x[i]);
+    free(x);
+    return 0;
 }
