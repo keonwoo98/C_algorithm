62923fe3932ee107619ebc683856e42a2adf075c
Keonwoo <keonwoo98@naver.com>
2021-02-08 16:59:08 +0900

    ch06

--- ch01-ch06/ch06.c
+++ ch01-ch06/ch06.c
@@ -1008,34 +1008,103 @@
 // }
 
 // 실습 6C-1
-#define swap(type, x, y) do{type t = x; x = y; y = t;} while(0)
+// #define swap(type, x, y) do{type t = x; x = y; y = t;} while(0)
 
+// void quick(int a[], int left, int right)
+// {
+//     int pl = left;
+//     int pr = right;
+//     int x = a[(pl + pr) / 2];
+
+//     int i;
+//     printf("a[%d] ~ a[%d] : { ", left, right);
+//     for(i = left; i < right; i++)
+//         printf("%d, ", a[i]);
+//     printf("%d }\n", a[right]);
+
+//     do
+//     {
+//         while(a[pl] < x) pl++;
+//         while(a[pr] > x) pr--;
+//         if(pl <= pr)
+//         {
+//             swap(int, a[pl], a[pr]);
+//             pl++;
+//             pr--;
+//         }
+//     } while (pl <= pr);
+    
+//     if(left < pr) quick(a, left, pr);
+//     if(pl < right) quick(a, pl, right);
+// }
+
+// int main(void)
+// {
+//     int i, nx;
+//     int *x;
+//     puts("퀵 정렬");
+//     printf("요소 개수 : ");
+//     scanf("%d", &nx);
+//     x = calloc(nx, sizeof(int));
+//     for(i = 0; i < nx; i++)
+//     {
+//         printf("x[%d] : ", i);
+//         scanf("%d", &x[i]);
+//     }
+
+//     quick(x, 0, nx - 1);
+//     puts("오름차순으로 정렬");
+//     for(i = 0; i < nx; i++)
+//         printf("x[%d] = %d\n", i, x[i]);
+    
+//     free(x);
+//     return 0;
+// }
+
+// 실습 6-10
+#include "IntStack.h"
+#define swap(type, x, y) do{type t = x; x = y; y = t;} while(0)
 void quick(int a[], int left, int right)
 {
-    int pl = left;
-    int pr = right;
-    int x = a[(pl + pr) / 2];
+    IntStack lstack;
+    IntStack rstack;
 
-    int i;
-    printf("a[%d] ~ a[%d] : { ", left, right);
-    for(i = left; i < right; i++)
-        printf("%d, ", a[i]);
-    printf("%d }\n", a[right]);
+    Initialize(&lstack, right - left + 1);
+    Initialize(&rstack, right - left + 1);
 
-    do
+    Push(&lstack, left);
+    Push(&rstack, right);
+
+    while(!IsEmpty(&lstack))
     {
-        while(a[pl] < x) pl++;
-        while(a[pr] > x) pr--;
-        if(pl <= pr)
+        int pl = (Pop(&lstack, &left), left);
+        int pr = (Pop(&rstack, &right), right);
+        int x = a[(left + right) / 2];
+        do
         {
-            swap(int, a[pl], a[pr]);
-            pl++;
-            pr--;
+            while(a[pl] < x) pl++;
+            while(a[pr] > x) pr--;
+            if(pl <= pr)
+            {
+                swap(int, a[pl], a[pr]);
+                pl++;
+                pr--;
+            }
+        } while (pl <= pr);
+        
+        if(left < pr)
+        {
+            Push(&lstack, left);
+            Push(&rstack, pr);
         }
-    } while (pl <= pr);
-    
-    if(left < pr) quick(a, left, pr);
-    if(pl < right) quick(a, pl, right);
+        if(pl < right)
+        {
+            Push(&lstack, pl);
+            Push(&rstack, pr);
+        }
+    }
+    Terminate(&lstack);
+    Terminate(&rstack);
 }
 
 int main(void)
