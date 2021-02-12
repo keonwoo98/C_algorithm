c602acaa67515d2b7cec9947cc3cb703cf19000e
Keonwoo <keonwoo98@naver.com>
2021-02-12 23:54:49 +0900

    ch06

--- ch01-ch06/ch06.c
+++ ch01-ch06/ch06.c
@@ -2016,79 +2016,79 @@
 // }
 
 // Q20
-static void memswap(void *x, void *y, size_t n)
-{
-    unsigned char *a = (unsigned char *)x;
-    unsigned char *b = (unsigned char *)y;
+// static void memswap(void *x, void *y, size_t n)
+// {
+//     unsigned char *a = (unsigned char *)x;
+//     unsigned char *b = (unsigned char *)y;
 
-    for(; n--; a++, b++)
-    {
-        unsigned char c = *a;
-        *a = *b;
-        *b = c;
-    }
-}
+//     for(; n--; a++, b++)
+//     {
+//         unsigned char c = *a;
+//         *a = *b;
+//         *b = c;
+//     }
+// }
 
-void q_sort(void *base, size_t nmemb, size_t size, int(*compar)(const void *, const void *))
-{
-    if(nmemb > 0)
-    {
-        size_t pl = 0;                      /* 왼쪽 커서 */
-        size_t pr = nmemb - 1;              /* 오른쪽 커서 */
-        size_t pv = nmemb;                  /* 피벗 */
-        size_t pt = (pl + pr) / 2;          /* 피벗 업데이트 */
-        char *v = (char *)base;             /* 첫 번째 요소에 대한 포인터 */
-        char *x;                            /* 피벗에 대한 포인터 */
+// void q_sort(void *base, size_t nmemb, size_t size, int(*compar)(const void *, const void *))
+// {
+//     if(nmemb > 0)
+//     {
+//         size_t pl = 0;                      /* 왼쪽 커서 */
+//         size_t pr = nmemb - 1;              /* 오른쪽 커서 */
+//         size_t pv = nmemb;                  /* 피벗 */
+//         size_t pt = (pl + pr) / 2;          /* 피벗 업데이트 */
+//         char *v = (char *)base;             /* 첫 번째 요소에 대한 포인터 */
+//         char *x;                            /* 피벗에 대한 포인터 */
 
-        do
-        {
-            if(pv != pt) x = &v[(pv = pt) * size];
-            while(compar((const void *)&v[pl * size], x) < 0) pl++;
-            while(compar((const void *)&v[pr * size], x) > 0) pr--;
-            if(pl <= pr)
-            {
-                pt = (pl == pv) ? pr : (pr == pv) ? pl : pv;
-                memswap(&v[pl * size], &v[pr * size], size);
-                pl++;
-                if(pr == 0)
-                    goto QuickRight;
-                pr--;
-            }
-        } while (pl <= pr);
+//         do
+//         {
+//             if(pv != pt) x = &v[(pv = pt) * size];
+//             while(compar((const void *)&v[pl * size], x) < 0) pl++;
+//             while(compar((const void *)&v[pr * size], x) > 0) pr--;
+//             if(pl <= pr)
+//             {
+//                 pt = (pl == pv) ? pr : (pr == pv) ? pl : pv;
+//                 memswap(&v[pl * size], &v[pr * size], size);
+//                 pl++;
+//                 if(pr == 0)
+//                     goto QuickRight;
+//                 pr--;
+//             }
+//         } while (pl <= pr);
         
-        if(0 < pr) q_sort(&v[0], pr + 1, size, compar);
-    QuickRight:
-        if(pl < nmemb - 1) q_sort(&v[pl * size], nmemb - pl, size, compar);
-    }
-}
+//         if(0 < pr) q_sort(&v[0], pr + 1, size, compar);
+//     QuickRight:
+//         if(pl < nmemb - 1) q_sort(&v[pl * size], nmemb - pl, size, compar);
+//     }
+// }
 
-int int_cmp(const int *a, const int *b)
-{
-    return *a < *b ? -1 : *a > *b ? 1 : 0;
-}
+// int int_cmp(const int *a, const int *b)
+// {
+//     return *a < *b ? -1 : *a > *b ? 1 : 0;
+// }
 
-int main(void)
-{
-    int i, nx;
-    int *x;
+// int main(void)
+// {
+//     int i, nx;
+//     int *x;
 
-    printf("q_sort 함수로 정렬합니다.\n");
-    printf("요솟수 : ");
-    scanf("%d", &nx);
-    x = calloc(nx, sizeof(int));
+//     printf("q_sort 함수로 정렬합니다.\n");
+//     printf("요솟수 : ");
+//     scanf("%d", &nx);
+//     x = calloc(nx, sizeof(int));
 
-    for(i = 0; i < nx; i++)
-    {
-        printf("x[%d] : ", i);
-        scanf("%d", &x[i]);
-    }
+//     for(i = 0; i < nx; i++)
+//     {
+//         printf("x[%d] : ", i);
+//         scanf("%d", &x[i]);
+//     }
 
-    q_sort(x, nx, sizeof(int), (int(*)(const void *, const void *))int_cmp);
+//     q_sort(x, nx, sizeof(int), (int(*)(const void *, const void *))int_cmp);
 
-    puts("오름차순으로 정렬");
-    for(i = 0; i < nx; i++)
-        printf("x[%d] : %d\n", i, x[i]);
+//     puts("오름차순으로 정렬");
+//     for(i = 0; i < nx; i++)
+//         printf("x[%d] : %d\n", i, x[i]);
 
-    free(x);
-    return 0;
-}
+//     free(x);
+//     return 0;
+// }
