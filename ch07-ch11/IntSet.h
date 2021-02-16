7cd449686cf80141493f1f1b3d09adbeaa0c0edf
Keonwoo <keonwoo98@naver.com>
2021-02-17 02:49:17 +0900

    IntSet

--- ch07-ch11/IntSet.h
+++ ch07-ch11/IntSet.h
@@ -12,6 +12,8 @@ int Initialize(IntSet *s, int max);
 
 int IsMember(const IntSet *s, int n);
 
+int IsFull(const IntSet *s);
+
 void Add(IntSet *s, int n);
 
 void Remove(IntSet *s, int n);
@@ -30,9 +32,24 @@ IntSet *Intersection(IntSet *s1, const IntSet *s2, const IntSet *s3);
 
 IntSet *Difference(IntSet *s1, const IntSet *s2, const IntSet *s3);
 
+IntSet *SymmetricDifference(IntSet *s1, const IntSet *s2, const IntSet *s3);
+
+IntSet *ToUnion(IntSet *s1, const IntSet *s2);
+
+IntSet *ToIntersection(IntSet *s1, const IntSet *s2);
+
+IntSet *ToDifference(IntSet *s1, const IntSet *s2);
+
+int IsSubset(const IntSet *s1, const IntSet *s2);
+
+int IsProperSubset(const IntSet *s1, const IntSet *s2);
+
 void Print(const IntSet *s);
 
 void PrintLn(const IntSet *s);
 
+void Clear(IntSet *s);
+
 void Terminate(IntSet *s);
+
 #endif
