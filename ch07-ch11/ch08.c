d56b64b80ea6e14e10ece1d48180759492faf676
Keonwoo <keonwoo98@naver.com>
2021-02-19 22:06:21 +0900

    ch08

--- ch07-ch11/ch08.c
+++ ch07-ch11/ch08.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include <limits.h>
 
 // 실습 8-1
@@ -141,21 +142,61 @@
 // }
 
 // 실습 8-6
-int str_len(const char *s)
-{
-    int len = 0;
+// int str_len(const char *s)
+// {
+//     int len = 0;
 
-    while(s[len])
-        len++;
-    return len;
-}
+//     while(s[len])
+//         len++;
+//     return len;
+// }
 
-int main(void)
-{
-    char str[256];
-    printf("문자열 : ");
-    scanf("%s", str);
-    printf("이 문자열의 길이는 %d입니다.\n", str_len(str));
+// int main(void)
+// {
+//     char str[256];
+//     printf("문자열 : ");
+//     scanf("%s", str);
+//     printf("이 문자열의 길이는 %d입니다.\n", str_len(str));
 
-    return 0;
-}
+//     return 0;
+// }
+
+// 실습 8-7
+// int str_len(const char *s)
+// {
+//     int len = 0;
+//     while(*s++)
+//         len++;
+//     return len;
+// }
+
+// int main(void)
+// {
+//     char str[256];
+//     printf("문자열 : ");
+//     scanf("%s", str);
+//     printf("이 문자열의 길이는 %d입니다.\n", str_len(str));
+
+//     return 0;
+// }
+
+// 실습 8-8
+// int str_len(const char *s)
+// {
+//     const char *p = s;
+//     while(*s)
+//         s++;
+//     return s - p;
+// }
+
+// int main(void)
+// {
+//     char str[256];
+//     printf("문자열 : ");
+//     scanf("%s", str);
+//     printf("이 문자열의 길이는 %d입니다.\n", str_len(str));
+
+//     return 0;
+// }
+
+// 실습 8-9
