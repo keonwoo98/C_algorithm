dda618212308e2cc3bdec06964a3a8ea6945e6b4
Keonwoo <keonwoo98@naver.com>
2021-03-03 18:18:48 +0900

    ch10

--- ch07-ch11/ch10.c
+++ ch07-ch11/ch10.c
@@ -5,7 +5,7 @@
 // 실습 10-3
 typedef enum
 {
-    TERMINATE, ADD, REMOVE, SEARCH, PRINT
+    TERMINATE, ADD, REMOVE, SEARCH, MIN, MAX, PRINT, PRINTREVERSE
 } Menu;
 
 Menu SelectMenu(void)
@@ -13,7 +13,7 @@ Menu SelectMenu(void)
     int ch;
     do
     {
-        printf("(1)삽입 (2)삭제 (3)검색 (4)출력 (0)종료 : ");
+        printf("(1)삽입 (2)삭제 (3)검색 (4)최솟값 (5)최댓값 (6)출력 (7)내림차순 출력 (0)종료 : ");
         scanf("%d", &ch);
     } while (ch < TERMINATE || ch > PRINT);
     return(Menu)ch;
@@ -45,9 +45,24 @@ int main(void)
                     PrintLnMember(&temp->data);
                 break;
 
+            case MIN :
+                puts("최솟값");
+                PrintLnMember(&(GetMinNode(root))->data);
+                break;
+
+            case MAX :
+                puts("최댓값");
+                PrintLnMember(&(GetMaxNode(root))->data);
+                break;
+
             case PRINT :
                 puts("모든 노드 출력");
                 PrintTree(root);
+                break;
+
+            case PRINTREVERSE :
+                puts("모든 노드 내림차순 출력");
+                PrintTreeReverse(root);
                 break;
         }
     } while (menu != TERMINATE);
