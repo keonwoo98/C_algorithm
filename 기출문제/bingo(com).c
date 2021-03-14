073ad92040441eb6e6fe3a0667b6658f9d6705a6
Keonwoo <keonwoo98@naver.com>
2021-03-14 23:12:39 +0900

    bingo(com)

--- 기출문제/bingo(com).c
+++ 기출문제/bingo(com).c
@@ -1,5 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-// #include <Windows.h>
+#include <Windows.h>
 
+void bingoGameStart(int *);
+void printRecord(int *);
+void init(int *);
+int checkBingo(int *);
+void printBingoBoard(int *);
+void inputNum(int *, int *);
+void randNum(int *, int *);
+
+int main(void)
+{
+    int menu;
+    int record[3] = { 0 };
+    while(1)
+    {
+        printf("1. 빙고 게임 시작\n");
+        printf("2. 전적 보기\n");
+        printf("3. 프로그램 종료\n");
+        printf("입력 : ");
+        scanf("%d", &menu);
+        switch(menu)
+        {
+            case 1 :
+                bingoGameStart(record);
+                break;
+            case 2 : 
+                printRecord(record);
+                break;
+            default :
+                return 0;
+        }
+        system("pause");
+        system("cls");
+    }
+    return 0;
+}
