027e342d261a9b9804d9cf4a000442c77f61daa0
Keonwoo <keonwoo98@naver.com>
2021-03-16 17:44:03 +0900

    phonebook

--- 기출문제/phonebook.c
+++ 기출문제/phonebook.c
@@ -1,47 +1,64 @@
 #include <stdio.h>
 #include <string.h>
 
-struct phone {
+typedef struct {
     char name[20];
     char number[20];
     int age;
-};
+} Phone;
 
-void phone_sort(struct phone[], int);       // 이름으로 오름차순 정렬
-void phone_print(struct phone[], int);      // 모든 전화번호 출력
+void phone_sort(Phone[], int);       // 이름으로 오름차순 정렬
+void phone_print(Phone[], int);      // 모든 전화번호 출력
+int SelectMenu(void)
+{
+    int menu;
+    do
+    {
+        printf("1. 등록할 정보 입력(최대 10명)\n");
+        printf("2. 등록한 정보 출력\n");
+        printf("0. 종료\n");
+        printf("번호를 입력하세요 : ");
+        scanf("%d", &menu);
+    } while (menu < 0 || menu > 2);
+    return menu;
+}
 
 int main()
 {
-    int i;
-    int count;                              // 전화번호 개수 저장할 변수
-    struct phone table[10];                 // 전화번호 최대 10개 저장할 구조체 배열 변수
-    printf("등록할 정보 입력(최대 10명)\n");
-    printf("그만 입력하려면 0 입력\n");
-    for(i = 0; i < 10; i++)
+    int cnt = 0;
+    int menu;
+    Phone table[10];                 // 전화번호 최대 10개 저장할 구조체 배열 변수
+    
+    do
     {
-        printf("이름 입력 : ");
-        scanf("%s", table[i].name);
-        if(strcmp(table[i].name, "0") == 0)
-            break;
-        printf("전화번호 입력 : ");
-        scanf("%s", table[i].number);
-        printf("나이 입력 : ");
-        scanf("%d", &table[i].age);
-        fflush(stdin);                      // 모든 입력 후 입력버퍼를 비워서 혹여나 남아있을 개행문자 제거
-    }
-    count = i;
-    phone_sort(table, count);
-    phone_print(table, count);
-    return 0;
+        switch(menu = SelectMenu())
+        {
+            case 1 :
+                printf("이름 입력 : ");
+                scanf("%s", table[cnt].name);
+                printf("전화번호 입력 : ");
+                scanf("%s", table[cnt].number);
+                printf("나이 입력 : ");
+                scanf("%d", &table[cnt].age);
+                cnt++;
+                fflush(stdin);                      // 모든 입력 후 입력버퍼를 비워서 혹여나 남아있을 개행문자 제거
+                break;
+
+            case 2 :
+                phone_sort(table, cnt);
+                phone_print(table, cnt);
+                break;
+        }
+    } while (menu != 0);
 }
 
-void phone_sort(struct phone table[], int count)
+void phone_sort(Phone table[], int cnt)
 {
     int i, j;
-    struct phone temp;                      // 교환을 위한 임시 구조체 변수
-    for(i = 0; i < count - 1; i++)          // i는 처음부터 마지막 전까지만 반복
-        for(j = i + 1; j < count; j++)      // j는 i + 1부터 마지막까지 반복
-            if(strcmp(table[i].name, table[j].name) == 1)
+    Phone temp;                      // 교환을 위한 임시 구조체 변수
+    for(i = 0; i < cnt - 1; i++)          // i는 처음부터 마지막 전까지만 반복
+        for(j = i + 1; j < cnt; j++)      // j는 i + 1부터 마지막까지 반복
+            if(strcmp(table[i].name, table[j].name) > 0)
             {   // 만약 i번째의 이름이 j번째보다 크다면 교환해서 값이 큰 이름을 뒤쪽으로 옮김
                 temp = table[i];
                 table[i] = table[j];
@@ -49,13 +66,13 @@ void phone_sort(struct phone table[], int count)
             }
 }
 
-void phone_print(struct phone table[], int count)
+void phone_print(Phone table[], int cnt)
 {
     int i;
     printf("\n------------------------------------\n");
     printf("%8s %16s %6s\n", "이름", "전화번호", "나이");
     printf("------------------------------------\n");
-    for (i = 0; i < count; i++)
+    for (i = 0; i < cnt; i++)
         printf("%8s %16s %6d\n", table[i].name, table[i].number, table[i].age);
     printf("------------------------------------\n\n");
 }
