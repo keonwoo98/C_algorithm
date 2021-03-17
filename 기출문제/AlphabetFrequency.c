73b1eaa2fcc413ee15000593d9f9c364c9df588e
Keonwoo <keonwoo98@naver.com>
2021-03-17 13:00:12 +0900

    AlphabetFrequency

--- 기출문제/AlphabetFrequency.c
+++ 기출문제/AlphabetFrequency.c
@@ -3,20 +3,20 @@
 
 int main()
 {
-    char str[255];
-    int len;
-    int cnt[26] = { 0 };
+    char str[255];                          // 사용자에게 문자열을 입력받아 저장할 변수
+    int len;                                // 입력받은 문자열의 길이를 저장할 변수
+    int cnt[26] = { 0 };                    // 문자열 내에 알파벳 빈도수를 저장할 배열
     int i;
     printf("문자열 입력 : ");
-    gets(str);
+    gets(str);                              // 공백 포함 문자열을 입력 받음
     len = strlen(str);
     for(i = 0; i < len; i++)
         if((str[i] >= 'A' && str[i] <= 'Z') || (str[i] >= 'a' && str[i] <= 'z'))
         {
-            if(str[i] <= 'Z')
-                cnt[str[i] - 'A']++;
+            if(str[i] <= 'Z')               // 'Z' = 90
+                cnt[str[i] - 'A']++;        // 'A' = 65
             else
-                cnt[str[i] - 'a']++;
+                cnt[str[i] - 'a']++;        // 'a' = 97
         }
     for(i = 0; i < 26; i++)
         if(cnt[i])
