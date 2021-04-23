90fc226149f55f7b06c3a84bd493564a56cf5c9c
Keonwoo <keonwoo98@naver.com>
2021-04-23 21:06:07 +0900

    ch02

---
+++ review/ch02/Q4.c
@@ -0,0 +1,32 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int	maxof(const int a[], int n)
+{
+	int	i;
+	int	max = a[0];
+	for (i = 1; i < n; i++)
+		if (max < a[i])
+			max = a[i];
+	return max;
+}
+
+int	main(void)
+{
+	int	i;
+	int	*height;
+	int	number;
+
+	srand(time(NULL));
+	number = 5 + rand() % 16;
+	height = calloc(number, sizeof(int));
+	for (i = 0; i < number; i++)
+	{
+		height[i] = 100 + rand() % 90;
+		printf("height[%d] = %d\n", i, height[i]);
+	}
+	printf("max height : %d\n", maxof(height, number));
+	free(height);
+	return 0;
+}
