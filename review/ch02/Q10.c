6635a8ea44f78b5b52516bad4eb1daf94761f444
Keonwoo <keonwoo98@naver.com>
2021-04-25 23:49:39 +0900

    ch02

---
+++ review/ch02/Q10.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define swap(type, x, y) do {type t = x; x = y; y = t;} while (0)
+
+void	shuffle(int a[], int n)
+{
+	int	i;
+
+	srand(time(NULL));
+	for (i = n - 1; i > 0; i--)
+	{
+		int j = rand() % (i + 1);
+		if (i != j)
+			swap(int, a[i], a[j]);
+	}
+}
+
+int	main(void)
+{
+	int	i;
+	int	*x;
+	int	nx;
+
+	printf("요솟수 : ");
+	scanf("%d", &nx);
+	x = calloc(nx, sizeof(int));
+
+	printf("%d개의 정수를 입력하세요.\n", nx);
+	for (i = 0; i < nx; i++)
+	{
+		printf("x[%d] : ", i);
+		scanf("%d", &x[i]);
+	}
+	shuffle(x, nx);
+	printf("배열 요소의 순서를 셔플했습니다.\n");
+	for (i = 0; i < nx; i++)
+		printf("x[%d] = %d\n", i, x[i]);
+	free (x);
+	return 0;
+}
+
+
