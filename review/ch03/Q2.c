53bfae762b7bc95ffe4992c5e3ba65b15dd8c065
Keonwoo <keonwoo98@naver.com>
2021-04-30 23:53:13 +0900

    ch03

---
+++ review/ch03/Q2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int	search(int a[], int n, int key)
+{
+	int	i;
+
+	printf("   |");
+	for (i = 0; i < n; i++)
+		printf("%3d", i);
+	printf("\n---+");
+	for (i = 0; i < n; i++)
+		printf("---");
+	printf("-\n");
