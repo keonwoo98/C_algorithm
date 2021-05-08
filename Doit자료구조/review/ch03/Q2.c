ee4b1c6d215f2e6b6cafa63d407bd87abd063274
Keonwoo <keonwoo98@naver.com>
2021-05-08 10:49:49 +0900

    C_algorithm

--- Doit자료구조/review/ch03/Q2.c
+++ Doit자료구조/review/ch03/Q2.c
@@ -12,3 +12,23 @@ int	search(int a[], int n, int key)
 	for (i = 0; i < n; i++)
 		printf("---");
 	printf("-\n");
+
+	for (i = 0; i < n; i++)
+	{
+		int j;
+		printf("   |");
+		printf("%*s", 3 * (i - 1) + 5, "");
+		printf("*\n");
+		printf("%3d|", i);
+		for (j = 0; j < n; j++)
+			printf("%3d", a[j]);
+		putchar('\n');
+
+		if (a[i] == key)
+			return i;
+
+		printf("   |\n");
+	}
+
+	return -1;
+}
