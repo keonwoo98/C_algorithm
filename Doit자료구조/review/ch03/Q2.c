8a480cf0d8037f86812c9d242a352023bcb36560
김건우 <keokim@gimgeon-uui-MacBookAir.local>
2021-05-10 22:47:09 +0900

    C_algorithm

--- Doit자료구조/review/ch03/Q2.c
+++ Doit자료구조/review/ch03/Q2.c
@@ -32,3 +32,32 @@ int	search(int a[], int n, int key)
 
 	return -1;
 }
+
+int main(void)
+{
+	int i, nx, ky, idx;
+	int *x;						/* 배열의 첫 번째 요소에 대한 포인터 */
+
+	puts("선형 검색");
+	printf("요솟수 : ");
+	scanf("%d", &nx);
+	x = calloc(nx, sizeof(int)); /* 요솟수 nx인 int 형 배열을 생성 */
+
+	for (i = 0; i < nx; i++) {
+		printf("x[%d] : ", i);
+		scanf("%d", &x[i]);
+	}
+	printf("검색 값 : ");
+	scanf("%d", &ky);
+
+	idx = search(x, nx, ky);	/* 배열 x에서 값이 ky 인 요소를 선형 검색 */
+
+	if (idx == -1)
+		puts("검색에 실패했습니다.");
+	else
+		printf("%d는 x[%d]에 있습니다.\n", ky, idx);
+
+	free(x); /* 배열을 해제 */
+
+	return 0;
+}
